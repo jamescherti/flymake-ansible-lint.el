77a4488e7d13b556db81c6b3edb40e3bca999dfa
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-09-22 12:32:29 -0400

    Release: 1.0.0

--- flymake-ansible-lint.el
+++ flymake-ansible-lint.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 0.9.9
+;; Version: 1.0.0
 ;; URL: https://github.com/jamescherti/flymake-ansible-lint.el
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "26.1"))
